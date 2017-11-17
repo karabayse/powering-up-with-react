84b7899f7741206d02a98a323fca6da6ad2a923f
Kara Bayse <karabayse@gmail.com>
2017-11-17 15:12:49 -0600

    React notes

---
+++ client.js
@@ -0,0 +1,13 @@
+// React is a JS library for building user interfaces (UIs)
+// Some people use it as the V in MVC (Model-View-Controller)
+// React was built so solve one problem:
+// Building large applications with data that changes over time
+// Conceived at Facebook:
+// Built to simplify the process of building complex UIs
+
+// Write React components
+// Render data to the page
+// Make components communicate
+// Handle user events
+// Capture user input
+// Talk to remote servers
